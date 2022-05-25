97c9f5fcd2b6f5631218fa8bc010fd6fc941029b
F3547441 Gabriel Oliveira da Silva Sergio <gabriel.sergio@bb.com.br>
2022-05-25 13:33:24 -0300

    fixed movement logic

--- src/Snake.ts
+++ src/Snake.ts
@@ -14,28 +14,27 @@ class Snake{
     }
 
     move(value: string): void{
-        let x = 0;
-        let y = 0;
+        let beforeX = 0;
+        let beforeY = 0;
         for(let i = 0; i < this.body.length; i++){
+            let afterX = this.body[i].getX();
+            let afterY = this.body[i].getY();
             if(i == 0){
-                x = this.body[i].getX();
-                y = this.body[i].getY();
-
                 if(value == "up"){
-                    this.body[i].setY(y - 1);
+                    this.body[i].setY(afterY - 1);
                 }else if(value == "down"){
-                    this.body[i].setY(y + 1);
+                    this.body[i].setY(afterY + 1);
                 }else if(value == "left"){
-                    this.body[i].setX(x - 1);
+                    this.body[i].setX(afterX - 1);
                 }else if(value == "right"){
-                    this.body[i].setX(x + 1);
+                    this.body[i].setX(afterX + 1);
                 }
             }else{
-                this.body[i].setPosition(x, y);
-                x = this.body[i].getX();
-                y = this.body[i].getY();
+                this.body[i].setX(beforeX);
+                this.body[i].setY(beforeY);
             }
-            
+            beforeX = afterX;
+            beforeY = afterY;
         }
     }
 }
